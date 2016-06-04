f5fb08a4b72ab7d71c1aca26af88ad8420c4110b
alimh <cocoamy6@hotmail.com>
2016-06-04 09:27:33 +0800

    first

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,9 +15,13 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :letter_opener
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+  config.after_initialize do
+    Pay2go.integration_mode = :development
+  end
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
